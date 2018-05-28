02b185df40b4ed22eb1004f6ec4fa8b60b7f0cec
Amit Cirt <siamitcs0@gmail.com>
2018-05-28 21:49:08 +0300

    fix wrong logger performance measurement

--- LogTesting/PerformanceTest.h
+++ LogTesting/PerformanceTest.h
@@ -2,16 +2,19 @@
 TEST(PerformanceTest, TEST_LOAD_METHOD_NAME) {
     XmlLogger xml;
     xml.load("Log.config");
-    Logger log(xml, 0, LOG_CONCURRENCY_LEVEL);
     constexpr int NUM_OF_MSG = 20000;
     const char *msg1 = "it is a test";
     const char *msg2 = "and is going to success";
     auto start = std::chrono::high_resolution_clock::now();
-    for (size_t i = 0; i < NUM_OF_MSG / 4; i++) {
-        log.info(msg1);
-        log.log(6, msg2);
-        log.debug(msg1);
-        log.log("info", msg2);
+    {
+        Logger log(xml, 0, LOG_CONCURRENCY_LEVEL);
+        for (size_t i = 0; i < NUM_OF_MSG / 4; i++)
+        {
+            log.info(msg1);
+            log.log(6, msg2);
+            log.debug(msg1);
+            log.log("info", msg2);
+        }
     }
     auto end = std::chrono::high_resolution_clock::now();
     constexpr double msgPerMilisec = LOAD_THRESHOLD;
