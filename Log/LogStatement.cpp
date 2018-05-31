84caf0582ded80ab0174d0746720563b5a238d3c
Amit Cirt <siamitcs0@gmail.com>
2018-05-31 19:03:04 +0300

    improve message handle

--- Log/LogStatement.cpp
+++ Log/LogStatement.cpp
@@ -7,6 +7,29 @@ namespace Sys
     namespace Logging
     {
 #define MAX_LOG_SIZE    4096
+#define MESSAGE_ID      0
+#define LEVEL_ID        4
+        char *addSpecialStr(char *str, int msgIndex, const char *msg, int levelIndex, const char *level)
+        {
+            char *temp;
+            if (msgIndex > levelIndex)
+            {
+                temp = replace(str, msgIndex, msg, 0);
+                str = temp;
+                if (levelIndex != -1)
+                {
+                    str = replace(str, levelIndex, level, 0);
+                    delete[] temp;
+                }
+            }
+            else
+            {
+                temp = replace(str, levelIndex, level, 0);
+                str = replace(temp, msgIndex, msg, 0);
+                delete[] temp;
+            }
+            return str;
+        }
         LogStatement::LogStatement(const PreMessage *preMessage, const char* msg, const String& level) :ILogStatement(preMessage), level(level)
         {
             register int l = strlen(msg) + 1;
@@ -29,6 +52,8 @@ namespace Sys
             int curStrIndex = 0;
             int index = 0;
             int countl = 3;
+            int messageIndex = -1;
+            int levelIndex = -1;
             while (s != e)
             {
                 IData* dat;
@@ -41,7 +66,7 @@ namespace Sys
                 {
                     dat = *s;
                 }
-                char * t = dat->getData(((PreMessage*)(this->msg))->getPaterenAt(index));
+                ++s;
                 int nonPatIndex = ((PreMessage*)(this->msg))->getIndexInNonPatteren(index);
                 if (curStrIndex < nonPatIndex)
                 {
@@ -50,24 +75,29 @@ namespace Sys
                     nPat += nonPatIndex - curStrIndex;
                     curStrIndex = nonPatIndex;
                 }
+                if (dat->getNumber() == MESSAGE_ID)
+                {
+                    messageIndex = fP - finalStr;
+                    index++;
+                    continue;
+                }
+                else if (dat->getNumber() == LEVEL_ID)
+                {
+                    levelIndex = fP - finalStr;
+                    index++;
+                    continue;
+                }
+                char * t = dat->getData(((PreMessage*)(this->msg))->getPaterenAt(index));
                 strcat(fP,t);
                 fP += strlen(t);
                 delete []t;
-                ++s;
                 index++;
             }
             delete [] nonPat;
             char *ret = createStr(finalStr);
-            char* temp = replace(ret, find(ret, "~!@"), message, 3);
+            char *temp = addSpecialStr(ret, messageIndex, message, levelIndex, level.c_str());
             delete[]ret;
             ret = temp;
-            auto indexName = find(ret, "@`~");
-            if (indexName != -1)
-            {
-                temp = replace(ret, indexName, level.c_str(), 3);
-                delete[]ret;
-                ret = temp;
-            }
             String r = createString(ret);
             delete[]ret;
             r.append("\n");
