d283f70195d4479b0be9d9a32fb50b04d1792ee8
Amit Cirt <siamitcs0@gmail.com>
2018-08-28 16:00:49 +0300

    fix pattern trim when the pattern ending with regular text(not special for the logger) this text was trimming and not get logged.

--- LogTesting/LogStatementTest.cpp
+++ LogTesting/LogStatementTest.cpp
@@ -17,4 +17,11 @@ namespace LogTesting
         expectedMsg.append(".message is : message\n");
         EXPECT_STREQ(expectedMsg.c_str(), ls.getMessage(pm.getNecessaryData()).c_str());
     }
+    TEST(LogStatementTest, testPatternTrim)
+    {
+        PreMessage pm("@[msg].", "logname");
+        LogStatement ls(&pm, "message", String("test_level"));
+        String expextedMsg = String("message.\n");
+        EXPECT_STREQ(expextedMsg.c_str(), ls.getMessage(pm.getNecessaryData()).c_str());
+    }
 }
