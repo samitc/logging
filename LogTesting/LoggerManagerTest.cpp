c44aad7683c47c7ceaf7be799abfa35775618775
Amit Cirt <siamitcs0@gmail.com>
2019-05-07 21:55:48 +0300

    Force order of logs in logger manager.

--- LogTesting/LoggerManagerTest.cpp
+++ LogTesting/LoggerManagerTest.cpp
@@ -12,8 +12,8 @@ namespace LogTesting
     namespace
     {
 #define LOGGER_PORT 2000
-#define sendLog(c) (c)->addRef();manager.log((c), "debug", createStr("test"), "", false);
-#define expectStr logMessage=listener.popMessage();EXPECT_STREQ("test\n",logMessage);delete[] logMessage;
+#define sendLog(c,msg) (c)->addRef();manager.log((c), "debug", createStr(msg), "", false);
+#define expectStr(msg) do {logMessage=listener.popMessage();} while(logMessage==nullptr);EXPECT_STREQ(msg,logMessage);delete[] logMessage;
         TEST(LoggerManagerTest, testSlowPrinterBug)
         {
             LoggerListener listener(LOGGER_PORT);
@@ -31,37 +31,42 @@ namespace LogTesting
                 PreMessage* pm1 = new PreMessage("@[msg]", "");
                 PreMessage* pm2 = new PreMessage("@[msg]", "");
                 LogFilter filter;
-                filter.setFilterFunction([](const auto&) {
-                    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                std::atomic<int> numOfArrive(0);
+                filter.setFilterFunction([&numOfArrive](const auto&str) {
+                    numOfArrive++;
+                    while (numOfArrive < 2);
+                    if (str == "2")
+                    {
+                        std::this_thread::sleep_for(std::chrono::milliseconds(25));
+                    }
                     return true;
                 });
                 Configuration* c = new Configuration(pm1, &nw, 255, std::map<String, int>(), std::vector<String>(), LogFilter(), 0);
                 Configuration* c1 = new Configuration(pm2, &nw, 255, std::map<String, int>(), std::vector<String>(), std::move(filter), 2);
                 c->clearRemoveWriter();
                 c1->clearRemoveWriter();
-                nw.setOneWriteSleep(std::chrono::milliseconds(50));
+                nw.setOneWriteSleep(std::chrono::milliseconds(100));
                 std::atomic<bool> startWrite(false);
                 nw.setCallbackBeforeWrite([&startWrite]() {
                     startWrite = true;
                 });
-                sendLog(c1);
-                sendLog(c1);
+                sendLog(c1, "1");
+                sendLog(c1, "2");
                 while (!startWrite);
-                sendLog(c);
-                sendLog(c);
-                sendLog(c);
+                sendLog(c, "3");
+                sendLog(c, "4");
+                sendLog(c, "5");
                 isFinished = true;
                 c1->removeRef();
                 c->removeRef();
                 timeoutThread.detach();
             }
-            std::this_thread::sleep_for(std::chrono::milliseconds(10));
             char* logMessage;
-            expectStr;
-            expectStr;
-            expectStr;
-            expectStr;
-            expectStr;
+            expectStr("1\n");
+            expectStr("2\n");
+            expectStr("3\n");
+            expectStr("4\n");
+            expectStr("5\n");
             EXPECT_EQ(nullptr, listener.popMessage());
         }
     }
